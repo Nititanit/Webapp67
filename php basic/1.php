1aa551695246d10a7b47430679227d82ca4d536e
Nititanit <nnititanit@gmail.com>
2024-09-12 15:26:34 +0700

    Add files via upload

---
+++ php basic/1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello php</title>
+</head>
+<body>
+    <?php
+        echo "Hello PHP";
+        echo "<br>";
+        echo "tjis is my first time to write PHP Language";
+    echo "<br>";
+    echo "<br>";
+        define("MINISIZE", 50);
+        echo MINISIZE;
+    ?>
+    
+</body>
+</html>
